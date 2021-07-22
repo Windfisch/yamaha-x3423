6c009240490cd5ce6035cc143e041f23eebe6e75
Florian Jung <flo@windfis.ch>
2021-07-22 04:15:06 +0200

    Add usb support

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -7,10 +7,21 @@ use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::digital::v1_compat::OldOutputPin;
 use rtic::app;
 use rtic::cyccnt::U32Ext;
-use stm32f1xx_hal::gpio::{gpioa::*, gpiob::*, gpioc::*, Input, Floating, Analog, Output, PushPull, OpenDrain, State};
+use stm32f1xx_hal::gpio::{gpioa::*, gpiob::*, gpioc::*, Input, Floating, Analog, Alternate, Output, PushPull, OpenDrain, State};
 use stm32f1xx_hal::spi;
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::delay::Delay;
+use stm32f1xx_hal::usb::{UsbBus, Peripheral, UsbBusType};
+use stm32f1xx_hal::adc::Adc;
+use stm32f1xx_hal::pac::ADC1;
+use mcp49xx::Mcp49xx;
+use mcp49xx::interface::SpiInterface;
+use mcp49xx::marker::Resolution12Bit;
+use mcp49xx::marker::DualChannel;
+use mcp49xx::marker::Unbuffered;
+
+use usb_device::bus;
+use usb_device::prelude::*;
 use core::convert::Infallible;
 
 use micromath::F32Ext;
@@ -111,11 +122,18 @@ impl X3423 {
 const APP: () = {
 	struct Resources {
 		led: PC13<Output<PushPull>>,
-		x3423: X3423
+		x3423: X3423,
+		usb_dev: UsbDevice<'static, UsbBusType>,
+		midi: usbd_midi::midi_device::MidiClass<'static, UsbBus<Peripheral>>,
+		adc: Adc<ADC1>,
+		dac: Mcp49xx<SpiInterface<spi::Spi<stm32f1xx_hal::pac::SPI2, spi::Spi2NoRemap, (PB13<Alternate<PushPull>>, spi::NoMiso, PB15<Alternate<PushPull>>), u8>, OldOutputPin<PC15<Output<PushPull>>>>, Resolution12Bit, DualChannel, Unbuffered>,
+		delay: stm32f1xx_hal::delay::Delay
 	}
 
-	#[init]
+	#[init(spawn=[xmain])]
 	fn init(cx: init::Context) -> init::LateResources {
+		static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<UsbBusType>> = None;
+
 		// Enable cycle counter
 		let mut core = cx.core;
 		core.DWT.enable_cycle_counter();
@@ -145,13 +163,45 @@ const APP: () = {
 			.into_push_pull_output_with_state(&mut gpioc.crh, State::Low);
 		led.set_low().unwrap();
 
+
+		// Configure USB
+		// BluePill board has a pull-up resistor on the D+ line.
+		// Pull the D+ pin down to send a RESET condition to the USB bus.
+		let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+		usb_dp.set_low().ok();
+		cortex_m::asm::delay(clocks.sysclk().0 / 100);
+		
+		let usb_dm = gpioa.pa11;
+		let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
+
+		let usb_pins = Peripheral {
+			usb: device.USB,
+			pin_dm: usb_dm,
+			pin_dp: usb_dp
+		};
+		*USB_BUS = Some( UsbBus::new(usb_pins) );
+		let usb_bus = USB_BUS.as_ref().unwrap();
+
+		let midi = usbd_midi::midi_device::MidiClass::new(usb_bus, 1, 1).unwrap();
+		let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
+			.manufacturer("Windfisch")
+			.product("Faderboard")
+			.serial_number("000000")
+			.device_class(usbd_midi::data::usb::constants::USB_CLASS_NONE)
+			.build();
+
+		// USB interrupt
+		core.NVIC.enable(stm32f1xx_hal::pac::Interrupt::USB_LP_CAN_RX0);
+
+		// Configure SPI
 		let mosi = gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh);
 		let sck = gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh);
 		let dac_cs: OldOutputPin<_> = gpioc.pc15.into_push_pull_output_with_state(&mut gpioc.crh, State::High).into();
 
 		let spi = spi::Spi::spi2(device.SPI2, (sck, spi::NoMiso, mosi), spi::Mode { phase : spi::Phase::CaptureOnFirstTransition, polarity : spi::Polarity::IdleLow }, 5.mhz(), clocks, &mut rcc.apb1);
-
 		let mut dac = mcp49xx::Mcp49xx::new_mcp4822(spi, dac_cs);
+
+		let mut adc = stm32f1xx_hal::adc::Adc::adc1(device.ADC1, &mut rcc.apb2, clocks);
 
 		let mut resources = init::LateResources {
 			led,
@@ -176,71 +226,83 @@ const APP: () = {
 					d6: gpiob.pb11.into_open_drain_output(&mut gpiob.crh),
 					d7: gpiob.pb12.into_open_drain_output(&mut gpiob.crh),
 				}
-			}
+			},
+			usb_dev,
+			midi,
+			adc,
+			dac,
+			delay
 		};
 
 		cortex_m::asm::delay(5);
-		resources.x3423.reset.set_high();
 
-		resources.x3423.set_motor_enable(0x1FFFF);
-		for frame in 0..100 {
+		cx.spawn.xmain();
+
+		resources
+	}
+
+	
+	#[task(resources = [x3423, adc, dac, delay], capacity=10, priority=1)]
+	fn xmain(mut c : xmain::Context) {
+		c.resources.x3423.reset.set_high();
+
+		c.resources.x3423.set_motor_enable(0x1FFFF);
+		for frame in 0..400 {
 			for i in 0..17 {
-				//dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value(i as u16 * 0xbFF / 16));
-				dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value(
+				c.resources.dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value(
 					((f32::sin((frame as f32 /200. + i as f32 / 17.) * core::f32::consts::PI * 2.) / 2. + 0.5 ) * 2600. + 256.) as u16
 				));
-				delay.delay_us(5_u16);
-				resources.x3423.capture_analog_value(i, &mut delay);
+				c.resources.delay.delay_us(5_u16);
+				c.resources.x3423.capture_analog_value(i, c.resources.delay);
 			}
 		}
 
-		resources.x3423.set_motor_enable( 0xa222 );
-
-		let mut adc = stm32f1xx_hal::adc::Adc::adc1(device.ADC1, &mut rcc.apb2, clocks);
-
+		c.resources.x3423.set_motor_enable( 0xa222 );
 		loop {
-			let fnord: u16 = adc.read(&mut resources.x3423.mfpos0).unwrap();
-			dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
-			delay.delay_us(5_u16);
-			resources.x3423.capture_analog_value(1, &mut delay);
+			let fnord: u16 = c.resources.adc.read(&mut c.resources.x3423.mfpos0).unwrap();
+			c.resources.dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
+			c.resources.delay.delay_us(5_u16);
+			c.resources.x3423.capture_analog_value(1, c.resources.delay);
 
-			let fnord: u16 = adc.read(&mut resources.x3423.mfpos1).unwrap();
-			dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
-			delay.delay_us(5_u16);
-			resources.x3423.capture_analog_value(5, &mut delay);
+			let fnord: u16 = c.resources.adc.read(&mut c.resources.x3423.mfpos1).unwrap();
+			c.resources.dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
+			c.resources.delay.delay_us(5_u16);
+			c.resources.x3423.capture_analog_value(5, c.resources.delay);
 
-			let fnord: u16 = adc.read(&mut resources.x3423.mfpos2).unwrap();
-			dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
-			delay.delay_us(5_u16);
-			resources.x3423.capture_analog_value(9, &mut delay);
+			let fnord: u16 = c.resources.adc.read(&mut c.resources.x3423.mfpos2).unwrap();
+			c.resources.dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
+			c.resources.delay.delay_us(5_u16);
+			c.resources.x3423.capture_analog_value(9, c.resources.delay);
 
-			let fnord: u16 = adc.read(&mut resources.x3423.mfpos3).unwrap();
-			dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
-			delay.delay_us(5_u16);
-			resources.x3423.capture_analog_value(13, &mut delay);
+			let fnord: u16 = c.resources.adc.read(&mut c.resources.x3423.mfpos3).unwrap();
+			c.resources.dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
+			c.resources.delay.delay_us(5_u16);
+			c.resources.x3423.capture_analog_value(13, c.resources.delay);
 
-			let fnord: u16 = adc.read(&mut resources.x3423.mfpos4).unwrap();
-			dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
-			delay.delay_us(5_u16);
-			resources.x3423.capture_analog_value(15, &mut delay);
+			let fnord: u16 = c.resources.adc.read(&mut c.resources.x3423.mfpos4).unwrap();
+			c.resources.dac.send(mcp49xx::Command::default().double_gain().channel(mcp49xx::Channel::Ch0).value( ((fnord as u32) *3895 / 4096 + 200) as u16));
+			c.resources.delay.delay_us(5_u16);
+			c.resources.x3423.capture_analog_value(15, c.resources.delay);
 		}
 	
 		
-		//delay.delay_ms(1000_u16);
-		resources.x3423.set_motor_enable(0);
+		//c.resources.delay.delay_ms(1000_u16);
+		c.resources.x3423.set_motor_enable(0);
+	}
 
-		/*for i in 0..17 {
-			resources.x3423.set_motor_enable(1 << i);
-			delay.delay_ms(2000_u16);
+	#[task(binds = USB_LP_CAN_RX0, spawn=[xmain], resources=[midi, usb_dev, led], priority=2)]
+	fn periodic_usb_poll(mut c : periodic_usb_poll::Context) {
+		c.resources.usb_dev.poll(&mut[c.resources.midi]);
+
+		let mut message: [u8; 4] = [0; 4];
+		while let Ok(len) = c.resources.midi.read(&mut message) {
+			if len == 4 {
+				c.resources.led.toggle();
+			}
 		}
-		resources.x3423.set_motor_enable(0);
-		*/
-		
-		/*delay.delay_ms(1000_u16);
-		resources.x3423.set_motor_enable(0x1FFFF);
-		delay.delay_ms(300_u16);
-		resources.x3423.set_motor_enable(0);*/
-
-		resources
+	}
+	
+	extern "C" {
+		fn EXTI0();
 	}
 };
